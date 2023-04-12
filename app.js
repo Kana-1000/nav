d7c8d0ec8df991927926ba261d2c0a7cf46f416d
Kana-1000 <128118357+Kana-1000@users.noreply.github.com>
2023-04-12 10:16:01 +0530

    Add files via upload
    3d nav bar

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+
